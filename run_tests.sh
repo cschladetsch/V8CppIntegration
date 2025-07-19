ce7400d7881c04122cf98f078f3c7b8c2cd06009
Christian Schladetsch <christian.schladetsch@gmail.com>
2025-07-19 19:17:47 +1000

    Code review and clean.

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Top-level test runner that calls the main test script in Shell/
-# This provides a convenient way to run tests from the project root
-
-exec ./shell_tests/run_tests.sh "$@"
