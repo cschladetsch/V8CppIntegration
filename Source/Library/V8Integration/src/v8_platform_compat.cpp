af97b885a092e055a404c424507902da793dd04f
Christian Schladetsch <christian.schladetsch@gmail.com>
2025-07-24 17:40:03 +1000

    Add subs

--- Source/Library/V8Integration/src/v8_platform_compat.cpp
+++ Source/Library/V8Integration/src/v8_platform_compat.cpp
@@ -11,7 +11,7 @@ std::unique_ptr<v8::Platform> CreateDefaultPlatform(int thread_pool_size) {
         thread_pool_size,
         v8::platform::IdleTaskSupport::kDisabled,
         v8::platform::InProcessStackDumping::kDisabled,
-        nullptr);
+        {});
 }
 
 }  // namespace v8_compat
